f2e71223b10f0577ac68cdba3c333f038a231d44
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-08 09:54:58 +0800

    home and about nav link added a smooth scrolling

--- js/script.js
+++ js/script.js
@@ -8,3 +8,24 @@ $(document).ready(function(){
       }, 800);
     });
   });
+
+// About Section
+
+  $(document).ready(function(){
+    $("#about-btn").click(function() {
+      $("html, body").animate({
+        scrollTop: $("#about").offset().top
+      }, 800);
+    });
+  });
+
+//  Home Section
+
+$(document).ready(function(){
+    $("#home-btn").click(function() {
+      $("html, body").animate({
+        scrollTop: $("#home").offset().top
+      }, 800);
+    });
+  });
+  
